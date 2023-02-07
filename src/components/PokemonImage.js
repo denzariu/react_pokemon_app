9aedc1ef7db9eb9ef793acb450d374614a08f7cf
Denzo <40743579+denzariu@users.noreply.github.com>
2023-02-07 12:23:32 +0200

    - Fixed image placeholder disappearing - Added a "Random Pokemon" button - Added "Previous" & "Next" buttons

--- src/components/PokemonImage.js
+++ src/components/PokemonImage.js
@@ -1,23 +1,43 @@
 import React from 'react'
 //import logo from '../../public/logo192.png'
 
+
+
+
+
 export default function PokemonImage({ pokemonName, pokemonImageUrl, loading }) {
+
+  const [loadedImage, setLoadImage] = React.useState(false)
+
+  React.useEffect(() => {
+    setLoadImage(false)
+  }, [pokemonImageUrl])
+
+  const handleLoadedImage = () => {
+    setLoadImage(true)
+  }
+
+  const image = <img src={pokemonImageUrl} className="illustration"
+                    alt="Illustration click to change Pokemon" 
+                    width="400px"
+                    onLoad={handleLoadedImage} />
+
+  const placeholder = <img src="/logo512.png" className="illustration"
+    alt="Illustration click to change Pokemon" 
+    width="400px" />
+
   return (
     <div>
-        {!loading && 
-            <img
-            src={pokemonImageUrl}
-            className="illustration"
-            alt="Illustration click to change Pokemon"
-            width="400px"
-            />
-        }
-        {loading &&
+        {(loading || !loadedImage) &&
             <img
             src="/logo512.png"
             width="400px"
             />
         }
+        {!loading &&
+            image
+        }
+        
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,28 +5,14 @@ import PokemonImage from "../components/PokemonImage";
 import { Link } from "wouter";
 import axios from 'axios'
 
-// Our language strings for the header
-const strings = [
-  "Hello React",
-  "Salut React",
-  "Hola React",
-  "안녕 React",
-  "Hej React"
-];
 
+const noMaxPokemon = 898
 const fileExtension = ".png"
-
-// Utility function to choose a random value from the language array
-function randomLanguage() {
-  return strings[Math.floor(Math.random() * strings.length)];
-}
-
 
 
 export default function Home() {
   
   const [pokemonID, setPokemonID] = React.useState("1");
-  const [hello, setHello] = React.useState(strings[0]);
   const [style, trigger] = useWiggle({ x: 5, y: 5, scale: 1 });
 
   
@@ -40,32 +26,44 @@ export default function Home() {
    
   React.useEffect(() => {
     setLoading(true)
-    let newPokemon = "https://pokeapi.co/api/v2/pokemon/" + pokemonID
+    const newPokemon = "https://pokeapi.co/api/v2/pokemon/" + pokemonID
 
     let cancel
     axios.get(newPokemon, {
         cancelToken: new axios.CancelToken(c => cancel = c)
     }).then(res => {
-        setLoading(false)
         setPokemonName(res.data.name.charAt(0).toUpperCase() + res.data.name.slice(1).toLowerCase())
         setPokemonImageUrl("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + pokemonID + fileExtension)
-      })
+        setLoading(false)
+    })
 
       return () => cancel()
 }, [pokemonID])
 
-  // When the user clicks we change the header language
-  const handleChangePokemon = () => {
+  const handlePrevPokemon = () => {
     console.log(pokemonID)
-    // Choose a new Hello from our languages
-    const newHello = randomLanguage();
+    const newPokemonID = (Number(pokemonID) - 1).toString()
+
+    // Call the function to set the state string in our component
+    setPokemonID(newPokemonID)
+  };
+
+  const handleNextPokemon = () => {
+    console.log(pokemonID)
     const newPokemonID = (Number(pokemonID) + 1).toString()
 
     // Call the function to set the state string in our component
     setPokemonID(newPokemonID)
   };
 
-  
+  const handleRandomPokemon = () => {
+    console.log(pokemonID)
+    // Get a random Pokemon in range [1, noMaxPokemon]
+    const newPokemonID = ( Math.floor((Math.random() * (noMaxPokemon - 1))) + 1).toString()
+
+    // Call the function to set the state string in our component
+    setPokemonID(newPokemonID)
+  };
 
   return (
     <>
@@ -77,19 +75,21 @@ export default function Home() {
             pokemonName={pokemonName}
             loading={loading}
         />
-        {/* <img
-          src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + pokemonID + fileExtension}
-          className="illustration"
-          onClick={handleChangePokemon}
-          alt="Illustration click to change Pokemon"
-          width="400px"
-        /> */}
       </animated.div>
       <div className="navigation">
-        <button className="btn--click-me" onClick={handleChangePokemon}>
-            Click me!
+        {pokemonID > 1 &&
+            <button className="btn--click-me" onClick={handlePrevPokemon}>
+                Previous Pokemon
+            </button>
+        }
+        {pokemonID < noMaxPokemon &&
+            <button className="btn--click-me" onClick={handleNextPokemon}>
+                Next Pokemon
+            </button>
+        }
+        <button className="btn--click-me" onClick={handleRandomPokemon}>
+                Random Pokemon
         </button>
-        
       </div>
       
     </>
